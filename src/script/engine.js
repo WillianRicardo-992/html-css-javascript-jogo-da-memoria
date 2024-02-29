313bc741d576c775cf2659d8d2a5e7dd7ec1e361
Willian Ricardo <willianricardoleal@gmail.com>
2024-02-29 12:58:06 -0300

    criacao do arquivo em js

--- src/script/engine.js
+++ src/script/engine.js
@@ -1,3 +1,44 @@
 const emojis = [
-
+    "😂",
+    "😂",
+    "😡",
+    "😡",
+    "💀",
+    "💀",
+    "👻",
+    "👻",
+    "🐶",
+    "🐶",
+    "🤡",
+    "🤡",
+    "🥶",
+    "🥶",
+    "👽",
+    "👽",
 ];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1)); 
+
+for(let i = 0; i < emojis.length; i++)
+{
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+    if(openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {}
